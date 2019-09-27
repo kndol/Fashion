01a8bb9b09d91ebd8add76b84153b389b9b1c1d3
idong00 <55074166+idong00@users.noreply.github.com>
2019-09-27 19:43:53 +0900

    김동환 작업사항
    Start씬 랭킹 UI 제작

--- Assets/Scripts/StartUI.cs
+++ Assets/Scripts/StartUI.cs
@@ -21,36 +21,31 @@ public class StartUI : FashionController
 
     void Start ()
     {
-        StartTutorial();
-    }
-
-    void StartTutorial()
-    {
         Init_Data();
         uiMenu = Instantiate<UIBuilder>(uiCanvasPrefab);
 
-		uiMenu.AddLabel("메뉴");
-		uiMenu.AddDivider();
-		uiMenu.AddButton("용어설명", ButtonTerms);
-		uiMenu.AddButton("튜토리얼", ButtonTutorial);
-		uiMenu.AddButton("테스트", ButtonTest);
-		uiMenu.AddButton("끝내기", ButtonExit);
-		uiMenu.Show();
+        uiMenu.AddLabel("메뉴");
+        uiMenu.AddDivider();
+        uiMenu.AddButton("용어설명", ButtonTerms);
+        uiMenu.AddButton("튜토리얼", ButtonTutorial);
+        uiMenu.AddButton("테스트", ButtonTest);
+        uiMenu.AddButton("끝내기", ButtonExit);
+        uiMenu.Show();
 
         uiTerm = Instantiate<UIBuilder>(uiCanvasPrefab);
 
         uiTerm.SetPaneWidth(980);
         uiTerm.AddLabel("<b용어 설명</b>");
         uiTerm.AddDivider();
-		uiTerm.AddLabel("항목\t\t내용", TextAnchor.MiddleLeft);
-		uiTerm.AddScrollView("<b>완성선</b>\t\t패턴의 완성선\n" +
-			"<b>안내선</b>\t\t완성선을 그리기 위한 보조선\n" +
-			"<b>안단선</b>\t\t안단 패턴을 표시하기 위한 선\n" +
-			"<b>절개선</b>\t\t가위질을 해야함을 나타내는 표시\n" +
-			"<b>꺾임선</b>\t\t접는 선 표시\n" +
-			"<b>스티치선</b>\t스티치의 시작 부분과 끝부분을 나타낸다.\n" +
-			"<b>등분선</b>\t\t등분을 표시하며 부호를 붙이는 경우도 있음", TextAnchor.UpperLeft, 320);
-		uiTerm.AddButton("메뉴로", ButtonMenu);
+        uiTerm.AddLabel("항목\t\t내용", TextAnchor.MiddleLeft);
+        uiTerm.AddScrollView("<b>완성선</b>\t\t패턴의 완성선\n" +
+            "<b>안내선</b>\t\t완성선을 그리기 위한 보조선\n" +
+            "<b>안단선</b>\t\t안단 패턴을 표시하기 위한 선\n" +
+            "<b>절개선</b>\t\t가위질을 해야함을 나타내는 표시\n" +
+            "<b>꺾임선</b>\t\t접는 선 표시\n" +
+            "<b>스티치선</b>\t스티치의 시작 부분과 끝부분을 나타낸다.\n" +
+            "<b>등분선</b>\t\t등분을 표시하며 부호를 붙이는 경우도 있음", TextAnchor.UpperLeft, 320);
+        uiTerm.AddButton("메뉴로", ButtonMenu);
     }
 
     public void ButtonMenu()
